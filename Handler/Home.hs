e6dcc785869c0d8e96814c97978b1834af982d62
Joe Grice <joeisnotamused@gmail.com>
2016-05-30 18:06:44 +0100

    Removed excess code/imports

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -6,20 +6,8 @@ module Handler.Home where
 {-# LANGUAGE TypeFamilies #-}
  
 import Yesod
-import Prelude (IO)
 import Import
-import Data.Text (Text)
-import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3,
-                              withSmallInput)
-import Text.Julius (RawJS (..))
 
--- This is a handler function for the GET request method on the HomeR
--- resource pattern. All of your resource patterns are defined in
--- config/routes
---
--- The majority of the code you will write in Yesod lives in these handler
--- functions. You can spread them across multiple files if you are so
--- inclined, or create a single monolithic file.
 getHomeR :: Handler Html
 getHomeR = defaultLayout $ do
         $(widgetFile "homepage")
